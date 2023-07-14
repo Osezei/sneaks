0e21d02973e04364d53066c371d4da6efbabb76d
osezei <john.osezei@yahoo.com>
2023-07-14 21:59:29 +0100

    about

--- pages/about.js
+++ pages/about.js
@@ -1,17 +1,44 @@
 import React from "react";
 import Layout from "@/components/Layout";
 import Image from "next/image";
+import Me from "../images/mee.jpeg";
 
 const About = () => {
   const activePage = "about";
   return (
     <Layout activePage={activePage}>
-      <section className="text-center text-2xl font-medium">
-        <h3>
-          This was created with love using nextjs and tailwindCSS
-          <br /> by
-          <br /> Osezei.
-        </h3>
+      <section className="flex justify-center items-center gap-x-6">
+        <div className="">
+          <Image width={500} height={500} className="" src={Me} alt="headboy" />
+        </div>
+        <div className="w-[420px]">
+          <p>
+            I am a Frontend Developer able to build a Web presence from the
+            ground concept, layout and UI to re-useable code, a goal-oriented
+            Frontend Developer with a strong commitment to collaboration and
+            solutions-oriented problem-solving.
+          </p>
+          <br />
+
+          <p>
+            Skilled at writing well-designed, testable and efficient code using
+            current best practices in development.
+          </p>
+          <br />
+
+          <p>
+            Fast learner, hard worker and team player ready to work in a
+            fast-paced, an collaborative environment.
+          </p>
+          <br />
+          <a
+            target="_blank"
+            href="https://docs.google.com/document/d/1jZWij4Y1c6x6Bt90_ofC3zqSh_KLU_Ha/edit?usp=drivesdk&ouid=106483839693778346197&rtpof=true&sd=true"
+            className="text-2xl uppercase font-bold underline-offset-8 hover:underline text-[#FF7E1B] text-center ease-in-out duration-300"
+          >
+            view resume
+          </a>
+        </div>
       </section>
     </Layout>
   );
--- pages/contact.js
+++ pages/contact.js
@@ -5,8 +5,21 @@ const Contact = () => {
   const activePage = "contact";
   return (
     <Layout activePage={activePage}>
-      <section>
-        <p>ehb hgerih</p>
+      <section className="text-center mt-28">
+        <h3 className=" uppercase text-xl text-text-color mb-2">
+          Send me a message
+        </h3>
+        <a
+          className=" text-5xl lg:text-6xl font-semibold underline underline-offset-4 mail text-[#FF7E1B] "
+          href="mailto:john.osezei@yahoo.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          john.osezei@yahoo.com
+        </a>
+        {/* <h3 className=" uppercase text-xl text-text-color mt-14 mb-2">
+          OR CONNECT WITH ME ON SOCIALS
+        </h3> */}
       </section>
     </Layout>
   );
